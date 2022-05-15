3ee8d90e028541705d83d6bf608463cd7db424cf
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-05-15 17:57:46 -0500

    etiquetas e ids

--- dynamics/sesion.php
+++ dynamics/sesion.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SESION</title>
+    <link href="../statics/styles/sesion.css" type="text/css" rel="stylesheet">
+</head>
+<body>
 <?php
     session_name("sesion");
     session_id("1");
@@ -11,41 +21,42 @@
 //esto aparece si el usuario se registra
     switch($_SESSION["casa"]){
         case "ajolotes":
-            echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los ajolotes
+            echo "<p class='title'> Bienvenid@ ". $_SESSION["apodo"]." a la casa de los ajolotes </p>
             <br><br>
-            <button><a href='./galeria.php'>Galería</a></button>
-            <button><a href='./sala.php'>Sala comun</a></button>
-            <button><a href='./avatar.php'>Personalizar avatar</a></button>
+            <button class='font2'><a href='./galeria.php'>Galería</a></button>
+            <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+            <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
             ";
             break;
         case "vaquitas":
-            echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de las vaquitas marinas
+            echo " <p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de las vaquitas marinas</p>
             <br><br>
-            <button><a href='./galeria.php'>Galería</a></button>
-            <button><a href='./sala.php'>Sala comun</a></button>
-            <button><a href='./avatar.php'>Personalizar avatar</a></button>
+            <button class='font2'><a href='./galeria.php'>Galería</a></button>
+            <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+            <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
             ";
             break;
         case "teporingos":
-                echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los teporingos
+                echo " <p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de los teporingos</p>
                 <br><br>
-                <button><a href='./galeria.php'>Galería</a></button>
-                <button><a href='./sala.php'>Sala comun</a></button>
-                <button><a href='./avatar.php'>Personalizar avatar</a></button>
+                <button class='font2'><a href='./galeria.php'>Galería</a></button>
+                <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+                <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
                 ";
                 break;
         default:
-            echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los halcones
+            echo " <p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de los halcones</p>
             <br><br>
-            <button><a href='./galeria.php'>Galería</a></button>
-            <button><a href='./sala.php'>Sala comun</a></button>
-            <button><a href='./avatar.php'>Personalizar avatar</a></button>
+            <button class='font2'><a href='./galeria.php'>Galería</a></button>
+            <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+            <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
             ";
+            break;
     }
 
         echo "
         <form action='./cerrarsesion.php' method='post' target='_self'>
-            <br><br><button>Cerrar sesion </button>
+            <br><br><button class='font2'>Cerrar sesion </button>
         </form>
     ";
     }
@@ -54,47 +65,48 @@
     else if (isset($_SESSION["apodo"])){
             switch($_SESSION["casa"]){
                 case "ajolotes":
-                    echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los ajolotes
+                    echo " <p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de los ajolotes</p>
                     <br><br>
-                    <button><a href='./galeria.php'>Galería</a></button>
-                    <button><a href='./sala.php'>Sala comun</a></button>
-                    <button><a href='./avatar.php'>Personalizar avatar</a></button>
+                    <button class='font2'><a href='./galeria.php'>Galería</a></button>
+                    <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+                    <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
                     ";
                     break;
                 case "vaquitas":
-                    echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de las vaquitas marinas
+                    echo "<p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de las vaquitas marinas</p>
                     <br><br>
-                    <button><a href='./galeria.php'>Galería</a></button>
-                    <button><a href='./sala.php'>Sala comun</a></button>
-                    <button><a href='./avatar.php'>Personalizar avatar</a></button>
+                    <button class='font2'><a href='./galeria.php'>Galería</a></button>
+                    <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+                    <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
                     ";
                     break;
                 case "teporingos":
-                        echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los teporingos
+                        echo " <p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de los teporingos</p>
                         <br><br>
-                        <button><a href='./galeria.php'>Galería</a></button>
-                        <button><a href='./sala.php'>Sala comun</a></button>
-                        <button><a href='./avatar.php'>Personalizar avatar</a></button>
+                        <button class='font2'><a href='./galeria.php'>Galería</a></button>
+                        <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+                        <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
                         ";
                         break;
                 default:
-                    echo "Bienvenid@ ". $_SESSION["apodo"]." a la casa de los halcones
+                    echo "<p class='title'>Bienvenid@ ". $_SESSION["apodo"]." a la casa de los halcones</p>
                     <br><br>
-                    <button><a href='./galeria.php'>Galería</a></button>
-                    <button><a href='./sala.php'>Sala comun</a></button>
-                    <button><a href='./avatar.php'>Personalizar avatar</a></button>
+                    <button class='font2'><a href='./galeria.php'>Galería</a></button>
+                    <button class='font2'><a href='./sala.php'>Sala comun</a></button>
+                    <button class='font2'><a href='./avatar.php'>Personalizar avatar</a></button>
                     ";
             }
         
             echo "
                 <form action='./cerrarsesion.php' method='post' target='_self'>
-                    <button>Cerrar sesion </button>
+                    <button class='font2'>Cerrar sesion </button>
                 </form>
             ";
         }
        
     else {
         header("location:./InicioSesion");
-    }
-    
+    }  
 ?>
+</body>
+</html>
